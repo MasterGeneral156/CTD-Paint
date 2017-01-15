c271bad6a880d870d3119c7431f560c39bbf18de
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2017-01-15 15:54:01 -0500

    Build 1.0.1

--- src/main/java/com/themastergeneral/ctdpaint/CTDPaint.java
+++ src/main/java/com/themastergeneral/ctdpaint/CTDPaint.java
@@ -10,13 +10,14 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 import com.themastergeneral.ctdpaint.proxy.CommonProxy;
 
-@Mod(modid = CTDPaint.MODID, name = CTDPaint.MODNAME, version = CTDPaint.VERSION, acceptedMinecraftVersions = CTDPaint.acceptedMinecraftVersions)
+@Mod(modid = CTDPaint.MODID, name = CTDPaint.MODNAME, version = CTDPaint.VERSION, acceptedMinecraftVersions = CTDPaint.acceptedMinecraftVersions, updateJSON = CTDPaint.updateJSON)
 public class CTDPaint 
 {
 	public static final String MODID = "ctdpaint";
     public static final String MODNAME = "CTD Paint";
     public static final String VERSION = "1.0";
     public static final String acceptedMinecraftVersions = "1.10.2";
+	public static final String updateJSON = "https://dl.dropboxusercontent.com/u/72961306/TMG%20Assets/Update%20JSONs/CTD-Paint.json";
     
     @Instance
     public static CTDPaint instance = new CTDPaint();
--- src/main/java/com/themastergeneral/ctdpaint/events/Painting.java
+++ src/main/java/com/themastergeneral/ctdpaint/events/Painting.java
@@ -408,5 +408,101 @@ public class Painting
 				}
 			}
 		}
+		if (block == Blocks.CARPET)
+		{
+			if (player.getHeldItem(EnumHand.MAIN_HAND) != null)
+			{
+				Item item = player.getHeldItem(EnumHand.MAIN_HAND).getItem();
+				if (item == ModItems.white)
+				{
+					world.setBlockState(event.getPos(), Blocks.CARPET.getStateFromMeta(0));
+					painted = true;
+				}
+				if (item == ModItems.orange)
+				{
+					world.setBlockState(event.getPos(), Blocks.CARPET.getStateFromMeta(1));
+					painted = true;
+				}
+				if (item == ModItems.magenta)
+				{
+					world.setBlockState(event.getPos(), Blocks.CARPET.getStateFromMeta(2));
+					painted = true;
+				}
+				if (item == ModItems.light_blue)
+				{
+					world.setBlockState(event.getPos(), Blocks.CARPET.getStateFromMeta(3));
+					painted = true;
+				}
+				if (item == ModItems.yellow)
+				{
+					world.setBlockState(event.getPos(), Blocks.CARPET.getStateFromMeta(4));
+					painted = true;
+				}
+				if (item == ModItems.lime)
+				{
+					world.setBlockState(event.getPos(), Blocks.CARPET.getStateFromMeta(5));
+					painted = true;
+				}
+				if (item == ModItems.pink)
+				{
+					world.setBlockState(event.getPos(), Blocks.CARPET.getStateFromMeta(6));
+					painted = true;
+				}
+				if (item == ModItems.gray)
+				{
+					world.setBlockState(event.getPos(), Blocks.CARPET.getStateFromMeta(7));
+					painted = true;
+				}
+				if (item == ModItems.silver)
+				{
+					world.setBlockState(event.getPos(), Blocks.CARPET.getStateFromMeta(8));
+					painted = true;
+				}
+				if (item == ModItems.cyan)
+				{
+					world.setBlockState(event.getPos(), Blocks.CARPET.getStateFromMeta(9));
+					painted = true;
+				}
+				if (item == ModItems.purple)
+				{
+					world.setBlockState(event.getPos(), Blocks.CARPET.getStateFromMeta(10));
+					painted = true;
+				}
+				if (item == ModItems.blue)
+				{
+					world.setBlockState(event.getPos(), Blocks.CARPET.getStateFromMeta(11));
+					painted = true;
+				}
+				if (item == ModItems.brown)
+				{
+					world.setBlockState(event.getPos(), Blocks.CARPET.getStateFromMeta(12));
+					painted = true;
+				}
+				if (item == ModItems.green)
+				{
+					world.setBlockState(event.getPos(), Blocks.CARPET.getStateFromMeta(13));
+					painted = true;
+				}
+				if (item == ModItems.red)
+				{
+					world.setBlockState(event.getPos(), Blocks.CARPET.getStateFromMeta(14));
+					painted = true;
+				}
+				if (item == ModItems.black)
+				{
+					world.setBlockState(event.getPos(), Blocks.CARPET.getStateFromMeta(15));
+					painted = true;
+				}
+				if (painted == true)
+				{
+					Random randomGenerator = new Random();
+					int randomInt = randomGenerator.nextInt(100);
+					if (randomInt == 1)
+					{
+						player.setHeldItem(EnumHand.MAIN_HAND, new ItemStack(ModItems.unused));
+					}
+				}
+			}
+		}
 	}
 }
