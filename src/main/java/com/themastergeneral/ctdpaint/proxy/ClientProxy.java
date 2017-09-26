1a5ee60d2ff05cbacc90aec4b21b4d94d66b931d
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-09-26 11:09:36 -0400

    Remove in-chat update notifier

--- src/main/java/com/themastergeneral/ctdpaint/proxy/ClientProxy.java
+++ src/main/java/com/themastergeneral/ctdpaint/proxy/ClientProxy.java
@@ -1,13 +1,5 @@
 package com.themastergeneral.ctdpaint.proxy;
 
-import com.themastergeneral.ctdpaint.CTDPaint;
-import com.themastergeneral.ctdpaint.VersionChecker;
-import com.themastergeneral.ctdpaint.events.UpdateNotify;
-
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.item.Item;
-import net.minecraftforge.client.model.ModelLoader;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -19,14 +11,9 @@ public class ClientProxy extends CommonProxy {
 
 	public void init(FMLInitializationEvent e) {
 		super.init(e);
-		MinecraftForge.EVENT_BUS.register(new UpdateNotify());
 	}
 
 	public void postInit(FMLPostInitializationEvent e) {
 		super.postInit(e);
-		CTDPaint.versionChecker = new VersionChecker();
-		Thread versionCheckThread = new Thread(CTDPaint.versionChecker,
-				"Version Check");
-		versionCheckThread.start();
 	}
 }
