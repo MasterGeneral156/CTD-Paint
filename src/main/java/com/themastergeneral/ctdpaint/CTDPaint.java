7aff11a20afc283e90b17eab38ec252f94b0c3a4
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-09-30 21:01:34 -0400

    Jar sign mod

--- src/main/java/com/themastergeneral/ctdpaint/CTDPaint.java
+++ src/main/java/com/themastergeneral/ctdpaint/CTDPaint.java
@@ -1,27 +1,29 @@
 package com.themastergeneral.ctdpaint;
 
+import net.minecraftforge.fml.common.FMLLog;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.Logger;
 
 import com.themastergeneral.ctdpaint.proxy.CommonProxy;
 
-@Mod(modid = CTDPaint.MODID, name = CTDPaint.MODNAME, version = CTDPaint.VERSION, acceptedMinecraftVersions = CTDPaint.acceptedMinecraftVersions, updateJSON = CTDPaint.updateJSON, dependencies = CTDPaint.DEPENDENCIES)
+@Mod(modid = CTDPaint.MODID, name = CTDPaint.MODNAME, certificateFingerprint = CTDPaint.Fingerprint, version = CTDPaint.VERSION, acceptedMinecraftVersions = CTDPaint.acceptedMinecraftVersions, updateJSON = CTDPaint.updateJSON, dependencies = CTDPaint.DEPENDENCIES)
 public class CTDPaint {
 	public static final String MODID = "ctdpaint";
 	public static final String MODNAME = "CTD Paint";
-	public static final String VERSION = "2.1";
+	public static final String VERSION = "2.1.1";
 	public static final String acceptedMinecraftVersions = "1.12.2";
 	public static final String updateJSON = "https://github.com/MasterGeneral156/Version/blob/master/CTD-Paint.json";
 	public static final String DEPENDENCIES = "required-after:ctdcore@[1.2,];";
+	public static final String Fingerprint = "441b509a0f58a0ef41aca8daf1be20d96287635e";
 
 	@Instance
 	public static CTDPaint instance = new CTDPaint();
@@ -49,5 +51,10 @@ public class CTDPaint {
 	public void postInit(FMLPostInitializationEvent e) {
 		proxy.postInit(e);
 		logger.info("CTD Paint has loaded successfully.");
+	}
+
+	@EventHandler
+	public void onFingerprintViolation(FMLFingerprintViolationEvent e) {
+		FMLLog.warning("Invalid fingerprint detected for CTD Paint! TheMasterGeneral will not support this version!");
 	}
 }
